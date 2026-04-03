364ae090eea52d5fd6f53f4419c84b883f92928f
Juan Cobo Betancourt <juan@neogranadina.org>
2026-04-02 20:14:06 -0700

    Entity explorer links: deep-link to graph view with ?vista=red
    Entity tooltip "Abrir en el explorador de relaciones" links to
    /entidad/{code}/?vista=red which auto-switches to graph view.
    Top-right button links to /explorar/entidades/ with name search.

--- src/js/entity.js
+++ src/js/entity.js
@@ -67,6 +67,11 @@ document.addEventListener('DOMContentLoaded', async function() {
   buildIntro(links);
   buildRoleFilters(links);
 
+  // Auto-switch to graph if ?vista=red
+  if (new URLSearchParams(window.location.search).get('vista') === 'red') {
+    setTimeout(function() { switchView('graph'); }, 0);
+  }
+
   // --- Intro sentence with view links ---
 
   function buildIntro(allLinks) {
@@ -478,7 +483,7 @@ document.addEventListener('DOMContentLoaded', async function() {
       var typeWord = (node.entityType === 'person') ? 'persona' : 'entidad';
       html += '<div class="graph-tooltip-actions">';
       html += 'Esta ' + typeWord + ' está conectada a otros ' + otherDocs + ' documento' + (otherDocs !== 1 ? 's' : '') + ' en Zasqua. ';
-      html += '<a href="/explorar/entidades/?q=' + encodeURIComponent(node.id) + '" class="graph-tooltip-btn">Abrir en el explorador de entidades</a>';
+      html += '<a href="/entidad/' + escapeHtml(node.id) + '/?vista=red" class="graph-tooltip-btn">Abrir en el explorador de relaciones</a>';
       html += '</div>';
     }
 
